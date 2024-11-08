dd62aa7d1190c2d52f538fc1b1bf77c7f7b77676
Gavin <gsstockwell@gmail.com>
2024-11-07 21:33:16 -0800

    bones of project

---
+++ asset/js/front-page.js
@@ -0,0 +1,13 @@
+//code+gaming = coolmathgames.com
+const container = document.querySelector('#container');
+
+const add = document.querySelector('#add');
+
+document.getElementById('add').addEventListener('click', function() {
+
+const waterIntake = getElementById('waterIntake');//add the stuff 
+//making clear by adding NOTHING (hope it works)
+
+const newDay = document.getElementById('container');
+const newElement = newDay.cloneNode(true);//if false we would have to tell what child where need to go
+document.getElementById('clone').appendChild(newElement);});
